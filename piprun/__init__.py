13e10eecffa5d1095c7474819bfe5a95ebae4bee
Alexey Kotlyarov <a@koterpillar.com>
2016-05-05 14:49:11 +1000

    Extract Environment class

--- piprun/__init__.py
+++ piprun/__init__.py
@@ -1,5 +1,6 @@
 """Piprun main module."""
 
+import hashlib
 import os
 import subprocess
 
@@ -13,6 +14,68 @@ def devnull():
 
 
 VIRTUALENV_DIR = os.path.expanduser('~/.piprun')
+
+
+class Environment(object):
+    """A Python virtual environment reference."""
+
+    def __init__(self, requirements, interpreter=None):
+        """
+        Create a virtual environment reference.
+
+        :param interpreter: Python interpreter to use
+        :param requirements: Requirements to install into the environment
+        """
+
+        self.requirements = requirements
+        self.interpreter = interpreter or None
+
+    def create(self):
+        """Create this virtual environment."""
+
+        virtualenv_args = ['virtualenv', '--quiet']
+        if self.interpreter:
+            virtualenv_args.append('--python={}'.format(self.interpreter))
+        virtualenv_args.append(self.path)
+        subprocess.check_call(virtualenv_args, stdout=devnull())
+
+        pip = os.path.join(self.binary_path, 'pip')
+        subprocess.check_call((pip, 'install') + tuple(self.requirements),
+                              stdout=devnull())
+
+    def activate(self):
+        """Activate this virtual environment for use in subprocesses."""
+
+        os.environ['VIRTUAL_ENV'] = self.path
+        os.environ['PATH'] = \
+            self.binary_path + ':' + os.environ.get('PATH', '')
+
+    @property
+    def exists(self):
+        """Whether this environment exists."""
+
+        return os.path.exists(self.path)
+
+    @property
+    def path(self):
+        """
+        Path to this virtual environment, based on the hash of the requirements
+        and the interpreter.
+        """
+
+        return os.path.join(VIRTUALENV_DIR, self.hash)
+
+    @property
+    def binary_path(self):
+        """Path to the binaries in the virtual environment."""
+
+        return os.path.join(self.path, 'bin')
+
+    @property
+    def hash(self):
+        """Hash of the environment requirements and interpreter."""
+
+        return str(hash((self.interpreter, tuple(self.requirements))))
 
 
 def split_args(args):
@@ -31,36 +94,6 @@ def split_args(args):
     return args[:reqs_end], args[reqs_end + 1:]
 
 
-def create_env(path, interpreter, requirements):
-    """
-    Create a virtual environment.
-
-    :param path: Path of the new virtual environment
-    :param interpreter: Python interpreter to use
-    :param requirements: Requirements to install into the environment
-    """
-
-    virtualenv_args = ['virtualenv', '--quiet']
-    if interpreter:
-        virtualenv_args.append('--python={}'.format(interpreter))
-    virtualenv_args.append(path)
-    subprocess.check_call(virtualenv_args, stdout=devnull())
-
-    pip = os.path.join(path, 'bin', 'pip')
-    subprocess.check_call((pip, 'install') + requirements,
-                          stdout=devnull())
-
-
-def activate_env(path):
-    """Set environment variables to activate the virtual environment."""
-
-    os.environ['VIRTUAL_ENV'] = path
-
-    old_path = os.environ.get('PATH', '')
-    env_bin = os.path.join(path, 'bin')
-    os.environ['PATH'] = env_bin + ':' + old_path
-
-
 def main(*args):
     """Main entry point."""
 
@@ -72,18 +105,14 @@ def main(*args):
     else:
         interpreter = None
 
-    requirements_hash = str(hash((interpreter, requirements)))
+    environment = Environment(
+        requirements=requirements,
+        interpreter=interpreter,
+    )
 
-    env_path = os.path.join(VIRTUALENV_DIR, requirements_hash)
+    if not environment.exists:
+        environment.create()
 
-    if not os.path.exists(env_path):
-        create_env(
-            env_path,
-            interpreter=interpreter,
-            requirements=requirements,
-        )
+    environment.activate()
 
-    activate_env(env_path)
-
-    env_python = os.path.join(env_path, 'bin', 'python')
-    os.execv(env_python, (env_python,) + args)
+    os.execvp('python', ('python',) + args)
